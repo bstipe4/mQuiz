80a9c35282c07e2b99effe6c1c5c4a455e052560
bstipe <sblaze00@fesb.hr>
2018-02-11 19:14:32 +0100

    Facebookv2

--- app/src/main/java/com/example/stipe/kviz/Kraj.java
+++ app/src/main/java/com/example/stipe/kviz/Kraj.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,10 +18,16 @@ import com.facebook.CallbackManager;
 
 import com.facebook.FacebookSdk;
 
+import com.facebook.share.model.ShareContent;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.ShareMediaContent;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Kraj extends AppCompatActivity {
 
@@ -95,11 +102,18 @@ public class Kraj extends AppCompatActivity {
         shareDialog = new ShareDialog(this);
 
 
-        Bitmap image=BitmapFactory.decodeResource(getResources(),R.drawable.logo);
+        //Bitmap image=BitmapFactory.decodeResource(getResources(),R.drawable.logo);
 
-        SharePhoto photo = new SharePhoto.Builder().setCaption("Prikljuci se i ti ovoj zabavnoj igrici!").setBitmap(image).build();
+        //SharePhoto photo = new SharePhoto.Builder().setCaption("Imam ostvarenih "+String.valueOf(score)+" bodova na mQuiz! Pridruzite se i vi ovoj zabavnoj igrici!").setBitmap(image).build();
 
-        SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo).build();
+        //SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo).build();
+
+
+        ShareLinkContent content = new ShareLinkContent.Builder()
+                .setQuote("Imam ostvarenih "+String.valueOf(score)+" bodova na mQuiz! Pridruzite se i vi ovoj zabavnoj igrici!")
+                .setContentUrl(Uri.parse("https://image.ibb.co/mdd9MS/logo.png"))
+                .build();
+
 
         shareButton.setShareContent(content);
     }
